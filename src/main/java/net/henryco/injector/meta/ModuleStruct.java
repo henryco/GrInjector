dc9db7d210b0cef9357d4b90c6984c91191e3cbe
henryco <henrycodev@gmal.com>
2017-12-19 11:34:25 +0100

    bug fix

--- src/main/java/net/henryco/injector/meta/ModuleStruct.java
+++ src/main/java/net/henryco/injector/meta/ModuleStruct.java
@@ -75,7 +75,6 @@ public final class ModuleStruct {
 	private void addIncluded(Module m) {
 		for (Class<?> icl : m.include()) {
 			ModuleStruct struct = new ModuleStruct(icl);
-			components.addAll(struct.components);
 			targets.addAll(struct.targets);
 			included.add(struct);
 		}
